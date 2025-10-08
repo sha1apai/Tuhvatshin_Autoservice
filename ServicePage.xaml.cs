7979b33ab11b536a18fcde0939f1c10aae277ef3
YamatoCPP <rrrr37920@gmail.com>
2025-10-08 17:43:28 +0500

    6

--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -20,6 +20,12 @@ namespace Tuhvatshin_Autoservice
     /// </summary>
     public partial class ServicePage : Page
     {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+        List<Service> CurrentPageList = new List<Service>();
+        List<Service> TableList;
         public ServicePage()
         {
             InitializeComponent();
@@ -30,6 +36,85 @@ namespace Tuhvatshin_Autoservice
             UpdateServices();
         }
 
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+            if (CountRecords%10>0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage= CountRecords / 10;
+            }
+
+            Boolean Ifupdate = true;
+
+            int min;
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for(int i = CurrentPage *10; i< min;i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch(direction)
+                {
+                    case 1:
+                        if(CurrentPage>0)
+                        {
+                            CurrentPage--; 
+                            min= CurrentPage*10+10<CountRecords?CurrentPage*10+10 : CountRecords;
+                            for(int i = CurrentPage*10;i<min;i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                        case 2:
+                        if(CurrentPage<CountPage-1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for(int i = CurrentPage*10;i<min;i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate=false;
+                        }
+                        break;
+                }
+            }
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+                for(int i=1; i<=CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+                min= CurrentPage * 10+10<CountRecords?CurrentPage*10+10:CountRecords;
+                TBCount.Text=min.ToString();
+                TBAllRecords.Text=" из "+ CountRecords.ToString();
+                ServiceListView.ItemsSource = CurrentPageList;
+                ServiceListView.Items.Refresh();
+            }
+        }
         private void UpdateServices()
         {
             var currentServices = Tuhvatshin_autoservisEntities.GetContext().Service.ToList();
@@ -63,12 +148,15 @@ namespace Tuhvatshin_Autoservice
             ServiceListView.ItemsSource=currentServices.ToList();
             if(RButtonDown.IsChecked.Value)
             {
-                ServiceListView.ItemsSource = currentServices.OrderByDescending(p=>p.Cost).ToList();
+                currentServices = currentServices.OrderByDescending(p=>p.Cost).ToList();
             }
             if(RButtonUP.IsChecked.Value)
             {
-                ServiceListView.ItemsSource= currentServices.OrderBy(p=>p.Cost).ToList();
+                currentServices= currentServices.OrderBy(p=>p.Cost).ToList();
             }
+            ServiceListView.ItemsSource = currentServices;
+            TableList = currentServices;
+            ChangePage(0, 0);
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -92,6 +180,48 @@ namespace Tuhvatshin_Autoservice
         private void RButtonDown_Checked(object sender, RoutedEventArgs e)
         {
             UpdateServices();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentService = (sender as Button).DataContext as Service;
+            var currentClientServices = Tuhvatshin_autoservisEntities.GetContext().ClientService.ToList();
+            currentClientServices = currentClientServices.Where(p => p.ServiceID == currentService.ID).ToList();
+            if (currentClientServices.Count != 0)
+                MessageBox.Show("Невозможно выполнить удаление, так как существует записи на эту услугу");
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Tuhvatshin_autoservisEntities.GetContext().Service.Remove(currentService);
+                        Tuhvatshin_autoservisEntities.GetContext().SaveChanges();
+                        ServiceListView.ItemsSource = Tuhvatshin_autoservisEntities.GetContext().Service.ToList();
+                        UpdateServices();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+
+                }
+            }
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString())-1);
         }
     }
 }
